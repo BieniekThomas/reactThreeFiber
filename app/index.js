79d92f063face17ce662751a42bcc57cab7e0ccc
Thomas Bieniek <thomas.bieniek@gmail.com>
2020-06-29 17:43:03 +0200

    wrapper now working with router + bumped three version bc its not working with three fiber

--- app/index.js
+++ app/index.js
@@ -3,26 +3,31 @@ import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 
 import "./index.scss";
+import "./navigation.scss";
 
-import Wrapper from "./util/wrapper/wrapper";
 import ExampleOne from "./examples/01_example/index";
 import HelloCube from "./examples/02_own-example/index";
 import SplitText from "./examples/03_split_text/index";
+import Wrapper from "./util/wrapper/wrapper";
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <div id="navigation">
+          <nav>
+            <Link to="/">HelloCubes</Link>
+            <Link to="example_1">Example_1</Link>
+            <Link to="splitText">SplitText</Link>
+          </nav>
+        </div>
         <Router>
-          <HelloCube path="home" />
-          <ExampleOne path="example_1" />
-          <SplitText path="splitText" />
+          <Wrapper path="/">
+            <HelloCube path="/" />
+            <ExampleOne path="example_1" />
+            <SplitText path="splitText" />
+          </Wrapper>
         </Router>
-        <Wrapper>
-          <HelloCube />
-          <ExampleOne />
-          <SplitText />
-        </Wrapper>
       </>
     );
   }
--- app/util/wrapper/wrapper.js
+++ app/util/wrapper/wrapper.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./wrapper.scss";
 
-export default (props) => {
-  return props.children.map((item) => {
-    return <div className="wrapper">{item}</div>;
-  });
+export default ({ children }) => {
+  if (children.length > 1) {
+    return children.map((item) => {
+      return <div className="wrapper">{item}</div>;
+    });
+  } else {
+    return <div className="wrapper">{children}</div>;
+  }
 };
